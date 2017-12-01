6f1ccfe6103895624ba5136b1fa4e2cc1e0af8aa
Nathan Ramaker <Nathan.ramaker@helpsystems.com>
2017-12-01 10:20:35 -0600

    measure precision and recall

--- SentimentClassification/src/main/java/com/uiuc/cs410/sentiment/analyzer/SentimentTester.scala
+++ SentimentClassification/src/main/java/com/uiuc/cs410/sentiment/analyzer/SentimentTester.scala
@@ -17,7 +17,8 @@ object SentimentTester {
   val TabDelimiter = "\t"
   val CommaDelimiter = ","
   
-  val ANALYSIS_BREAK = " ################################# RESULTS ################################# "
+  val ANALYSIS_BREAK = " ################################# OVERALL ACCURACY ################################# "
+  val ANALYSIS_BREAK_MAJORITY = " ################################# MAJORITY CLASS ACCURACY ################################# "
   
   def main(args: Array[String]): Unit = {
     
@@ -36,7 +37,7 @@ object SentimentTester {
     
     var count = 0
     
-    unscoredExamples.slice(0,1000).foreach { example =>
+    unscoredExamples.foreach { example =>
       println("Example "+count)
       var tuple = scoreBoth(example.text, stanfordCorePredictor, naiveBayesPredictor)
       println("True score = "+example.score)
@@ -82,49 +83,116 @@ object SentimentTester {
     
     println(ANALYSIS_BREAK)
     
+    //stanford scores
     var correctStanford = 0.0
     var tooLowStanford = 0.0
     var muchTooLowStanford = 0.0
     var tooHighStanford = 0.0
     var muchTooHighStanford = 0.0
+    var stanfordMajCorrect = 0
+    var stanfordMajFalsePos = 0
+    var stanfordMajFalseNeg = 0
     
+    //naive bayes scores
     var correctNaiveBayes = 0.0
     var tooLowNaiveBayes = 0.0
     var muchTooLowNaiveBayes = 0.0
     var tooHighNaiveBayes = 0.0
     var muchTooHighNaiveBayes = 0.0
+    var naiveBayesMajCorrect = 0
+    var naiveBayesMajFalsePos = 0
+    var naiveBayesMajFalseNeg = 0
     
+    //overall 
     var totalExamples = 0.0
+    var counts = Array(0,0,0,0,0)
+    var majorityClass = -1
+    var majorityClasscount = 0
     
+    //determine majority class
     results.asScala.toArray.foreach { result =>
       totalExamples += 1.0
-      if(result.score.toDouble == result.stanfordPrediction)
+      var score = result.score
+      counts(score) += 1
+    }
+    
+     for ( i <- 1 to (counts.length - 1) ) {
+         if (counts(i) > majorityClasscount) {
+           majorityClass = i
+           majorityClasscount = counts(i);
+         }
+     }
+   
+    results.asScala.toArray.foreach { result =>
+      
+      if(result.score == majorityClasscount){
+        
+      }
+      
+      //Analyze StanfordCore
+      //we got it right
+      if(result.score.toDouble == result.stanfordPrediction){
         correctStanford+=1.0
-      else if(result.score.toDouble > result.stanfordPrediction){
-        tooLowStanford+=1.0
-        if(result.score.toDouble - result.stanfordPrediction > 1)
-          muchTooLowStanford+=1.0
+      }
+      //we got it wrong
+      else {
+        if(result.score.toDouble > result.stanfordPrediction){
+            if(result.score.toDouble - result.stanfordPrediction > 1)
+              muchTooLowStanford+=1.0
+        }
+        else{
+          tooHighStanford+=1.0
+          if(result.stanfordPrediction - result.score.toDouble > 1)
+            muchTooHighStanford+=1.0
+        }
+      }
+      
+      if(result.score == majorityClass){
+         if(result.stanfordPrediction == majorityClass)
+            stanfordMajCorrect+=1
+         else
+            stanfordMajFalseNeg+=1
       }
       else{
-        tooHighStanford+=1.0
-        if(result.stanfordPrediction - result.score.toDouble > 1)
-          muchTooHighStanford+=1.0
+        if(result.stanfordPrediction == majorityClass){
+          stanfordMajFalsePos+=1
+        }
       }
-      if(result.score.toDouble == result.naiveBayesPrediction)
+
+      
+      //Analyze NB
+      //we got it right
+      if(result.score.toDouble == result.naiveBayesPrediction){
         correctNaiveBayes+=1.0
-      else if(result.score.toDouble > result.naiveBayesPrediction){
-        tooLowNaiveBayes+=1.0
-        if(result.score.toDouble - result.naiveBayesPrediction > 1)
-          muchTooLowNaiveBayes+=1.0
+      }
+      //we got it wrong
+      else {
+        if(result.score.toDouble > result.naiveBayesPrediction){
+            if(result.score.toDouble - result.naiveBayesPrediction > 1)
+              muchTooLowNaiveBayes+=1.0
+        }
+        else{
+          tooHighStanford+=1.0
+          if(result.naiveBayesPrediction - result.score.toDouble > 1)
+            muchTooHighNaiveBayes+=1.0
+        }
+      }
+      
+      if(result.score == majorityClass){
+         if(result.naiveBayesPrediction == majorityClass)
+            naiveBayesMajCorrect+=1
+         else
+            naiveBayesMajFalseNeg+=1
       }
       else{
-        tooHighNaiveBayes+=1.0
-        if(result.naiveBayesPrediction - result.score.toDouble > 1)
-          muchTooHighNaiveBayes+=1.0
+        if(result.naiveBayesPrediction == majorityClass){
+          naiveBayesMajFalsePos+=1
+        }
       }
     }
     
-    println("Stanford CoreNLP Accuracy: "+correctStanford/totalExamples)
+    //print results
+    println("Stanford CoreNLP Exactly Accurately: "+correctStanford/totalExamples)
     println("     "+correctStanford.toInt+"/"+totalExamples.toInt)
     println("Stanford CoreNLP Scores Too Highly: "+tooHighStanford/totalExamples)
     println("     "+tooHighStanford.toInt+"/"+totalExamples.toInt)
@@ -135,7 +203,7 @@ object SentimentTester {
     println("Stanford CoreNLP Scores Much Too Low: "+muchTooLowStanford/totalExamples)
     println("     "+muchTooLowStanford.toInt+"/"+totalExamples.toInt)
     println("")
-    println("NaiveBayes Accuracy: "+correctNaiveBayes/totalExamples)
+    println("NaiveBayes Scores Exactly Accurately: "+correctNaiveBayes/totalExamples)
     println("     "+correctNaiveBayes.toInt+"/"+totalExamples.toInt)
     println("NaiveBayes Scores Too Highly: "+tooHighNaiveBayes/totalExamples)
     println("     "+tooHighNaiveBayes.toInt+"/"+totalExamples.toInt)
@@ -147,5 +215,17 @@ object SentimentTester {
     println("     "+muchTooLowNaiveBayes.toInt+"/"+totalExamples.toInt)
     println("")
     
+    println(ANALYSIS_BREAK_MAJORITY)
+    println("Majority Class: "+majorityClass)
+    println("Majority Class Count: "+majorityClasscount)
+    println("Majority Class Percentage: "+majorityClasscount/totalExamples)
+    println("")
+    println("Stanford Majority Predictions: "+stanfordMajCorrect+stanfordMajFalsePos)
+    println("Stanford Majority Precision: "+stanfordMajCorrect.toDouble/(stanfordMajCorrect+stanfordMajFalsePos))
+    println("Stanford Majority Recall: "+stanfordMajCorrect.toDouble/(stanfordMajCorrect+stanfordMajFalseNeg))
+    println("")
+    println("NaiveBayes Majority Predictions: "+naiveBayesMajCorrect+naiveBayesMajFalsePos)
+    println("NaiveBayes Majority Precision: "+naiveBayesMajCorrect.toDouble/(naiveBayesMajCorrect+naiveBayesMajFalsePos))
+    println("NaiveBayes Majority Recall: "+naiveBayesMajCorrect.toDouble/(naiveBayesMajCorrect+naiveBayesMajFalseNeg))
   }
 }
