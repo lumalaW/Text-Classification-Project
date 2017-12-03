c5a47f1df75d1153802ac754f8fe78ffba3e9107
Nathan Ramaker <Nathan.ramaker@helpsystems.com>
2017-12-03 08:19:21 -0600

    additional work

---
+++ EmailRouter/src/main/java/com/uiuc/cs410/sentiment/props/PropertyHandler.java
@@ -0,0 +1,29 @@
+package com.uiuc.cs410.sentiment.props;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class PropertyHandler {
+
+	private static final String GENERAL_PROPS_FILE = "conf/general.properties";
+	
+	public static final String SENTIMENT_VERY_NEG = "angry";
+	public static final String SENTIMENT_NEG = "negative";
+	public static final String SENTIMENT_NEUT = "neutral";
+	public static final String SENTIMENT_POS = "positive";
+	public static final String SENTIMENT_VERY_POS = "elated";
+	
+	public static final String LIST_TO="tolist";
+	public static final String LIST_CC="cclist";
+	public static final String LIST_BCC="bcclist";
+	
+	private Map<String, Properties> propertyFiles = new HashMap<>();
+	
+	public String getMailingList(String labelClass, String sentiment, String listLevel){
+		return null;
+	}
+	private Properties loadPropsFile(String name, String path){
+		return null;
+	}
+}
--- SentimentClassification/src/main/java/com/uiuc/cs410/sentiment/analyzer/SentimentTester.scala
+++ SentimentClassification/src/main/java/com/uiuc/cs410/sentiment/analyzer/SentimentTester.scala
@@ -37,7 +37,7 @@ object SentimentTester {
     
     var count = 0
     
-    unscoredExamples.slice(0,1000).foreach { example =>
+    unscoredExamples.foreach { example =>
       println("Example "+count)
       var tuple = scoreBoth(example.text, stanfordCorePredictor, naiveBayesPredictor)
       println("True score = "+example.score)
