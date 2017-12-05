e16eba079cfc5b360319f67d512738785218ca23
Nathan Ramaker <Nathan.ramaker@helpsystems.com>
2017-12-05 11:51:05 -0600

    working classify route

--- App.py
+++ App.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
+import json
 
 from TextClassification.Classify import classify
+import TextClassification.CollectExamples as collector
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'TheSecretKey'
@@ -16,6 +18,42 @@ def index():
         return render_template('classifiedText.html', text=category, entered_text=entered_text)
 
     return render_template("index.html")
+
+@app.route('/classifyText', methods=['GET'])
+def classifyText():
+	text = request.args.get('text')
+	print("TEXT = ", text)
+	ret = {}
+	if(len(text)==0):
+		print("ERROR")
+		ret['status']='failed'
+		ret['message']='text parameter is required'
+		return json.dumps(ret)
+	print("About to classify...")
+	category = classify(text)
+	print("Classification done: ", category)
+	ret['status']='success'
+	ret['message']='Classification successful'
+	ret['label']=category
+	return json.dumps(ret)
+
+
+@app.route('/train', methods=['POST'])
+def train():
+	examples_dir = request.args.get('examples_dir')
+	target_data_csv = './data.csv'
+	target_labels_csv = './labels.csv'
+	labels = collector.examine_samples(target_data_csv, target_labels_csv)
+
+	ret = {}
+	if(len(labels)==0):
+		ret['status']='failed'
+		ret['message']='Unable to parse sample documents. Training aborted.'
+		return json.dumps(ret)
+	#TODO: train model again
+	#TODO: pass it in to some other method
+	ret['labels']=labels
+	return json.dumps(ret)
 
 if __name__ == "__main__":
     app.run(debug=True)
